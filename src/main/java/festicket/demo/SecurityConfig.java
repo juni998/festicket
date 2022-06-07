2b95d450269d0ab4a9f4496f61eb7e06a71c3acf
aou101 <wodnrdlgla@gmail.com>
2022-06-07 20:38:47 +0900

    #4 spring security config

---
+++ src/main/java/festicket/demo/SecurityConfig.java
@@ -0,0 +1,61 @@
+package festicket.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class
+SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/resources/**");
+    }
+
+    /*권한 설정*/
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1234")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /* 인가 */
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        /* 로그인 */
+        http
+                .formLogin()
+                .defaultSuccessUrl("/index")
+                .failureUrl("/login")
+
+                .permitAll();
+
+        /* 로그아웃 */
+        http
+                .logout()
+                .logoutUrl("/logout");
+
+        return http.build();
+    }
+
+}
