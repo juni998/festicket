b05ac53bfc78590a29d23e013a52256890fd9600
juni998 <wodnrdlgla@gmail.com>
2022-07-01 16:26:04 +0900

    testBasicCRUD, findByAccount

--- src/main/java/festicket/demo/member/MemberRepository.java
+++ src/main/java/festicket/demo/member/MemberRepository.java
@@ -2,9 +2,13 @@ package festicket.demo.member;
 
 import festicket.demo.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Member findByAccount(@Param("account") String account);
 
 }
--- src/test/java/festicket/demo/member/MemberRepositoryTest.java
+++ src/test/java/festicket/demo/member/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -39,13 +40,30 @@ class MemberRepositoryTest {
 
     @Test
     public void basicCRUD() {
+        Member member1 = new Member("test", "1234", "테스트", "test@test.com", "1234");
+        Member member2 = new Member("test2", "1234", "테스트2", "test2@test.com", "1234");
+        Member member3 = new Member("test3", "1234", "테스트3", "test3@test.com", "1234");
+        Member member4 = new Member("test4", "1234", "테스트4", "test4@test.com", "1234");
 
-        //단건 조획 검증
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+        //단건 조회 검증
+        Member findAccount = memberRepository.findByAccount(member2.getAccount());
+        assertThat(findAccount).isEqualTo(member2);
 
         //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(4);
 
         //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(4);
 
         //삭제 검증
+        memberRepository.delete(member3);
+        long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(3);
     }
 }
