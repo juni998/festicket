50ca622f92bf4e1805b0fe773db983384d53d766
juni998 <wodnrdlgla@gmail.com>
2022-07-06 15:42:13 +0900

    테스트 계정 추가

--- src/main/java/festicket/demo/security/config/SecurityConfig.java
+++ src/main/java/festicket/demo/security/config/SecurityConfig.java
@@ -11,15 +11,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
+
+        auth.inMemoryAuthentication()
+                .withUser("test").password(passwordEncoder().encode("1234"))
+                .roles("USER");
     }
 
     //CustomAuthenticationProvider 인증처리
